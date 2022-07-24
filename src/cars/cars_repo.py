2bf869849e0451d24b68bc3189b6c54e038a1695
Stanico <ashadknowles@gmail.com>
2022-07-24 12:56:30 -0400

    add types for all repo and repo functions

--- src/cars/cars_repo.py
+++ src/cars/cars_repo.py
@@ -3,10 +3,10 @@ from src.cars.dto.cars_dto import CarsDto
 from src.database.database import db
 
 class CarRepo():
-    def __init__(self):
+    def __init__(self) -> None:
         self.db = db
 
-    def add_car(self, car: CarsDto):
+    def add_car(self, car: CarsDto) -> CarsDto:
         car = Car(car.license_plate, car.color, car.is_clean, car.hours, car.price)
         self.db.session.add(car)
         self.db.session.commit()
@@ -15,13 +15,13 @@ class CarRepo():
     def get_all_cars(self) -> list[CarsDto]:
         return list(map(lambda car: self.model_object_to_cars_dto(car=car), Car.query.all()))
     
-    def get_car_by_license_plate(self, license_plate):
+    def get_car_by_license_plate(self, license_plate: str) -> CarsDto:
         car: Car = Car.query.filter_by(license_plate=license_plate).first()
         if car:
             return self.model_object_to_cars_dto(car=car)
         return None
     
-    def update_car(self, updated_car: CarsDto):
+    def update_car(self, updated_car: CarsDto) -> CarsDto:
         car: Car = Car.query.filter_by(license_plate=updated_car.license_plate).first()
         if car:
             car.color = updated_car.color
@@ -32,7 +32,7 @@ class CarRepo():
             return self.model_object_to_cars_dto(car=car)
         return None
 
-    def delete_car(self, license_plate):
+    def delete_car(self, license_plate: str) -> CarsDto:
         car = Car.query.filter_by(license_plate=license_plate).first()
         self.db.session.delete(car)
         self.db.session.commit()
--- src/cars/cars_service.py
+++ src/cars/cars_service.py
@@ -1,3 +1,4 @@
+from typing import Union
 from src.cars.cars_repo import CarRepo
 from src.cars.dto.cars_dto import CarsDto
 from src.cars.enums.cars_custom_exceptions import CarsCustomExceptions
@@ -6,13 +7,13 @@ from werkzeug.exceptions import BadRequest, NotFound
 
 
 class CarService():
-    def __init__(self):
+    def __init__(self) -> None:
         self.car_repo = CarRepo()
     
     def build_car_dto(self, car: dict) -> CarsDto:
         return CarsDto(license_plate=car['license_plate'], color=car['color'], is_clean=car['is_clean'], hours=car['hours'])
 
-    def add_car(self, car: CarsDto):
+    def add_car(self, car: CarsDto) -> CarsDto:
         self._check_if_color_is_present(car.color)
         self._check_if_is_clean_is_present(car.is_clean)
         self._check_if_hours_is_present(car.hours)
@@ -22,17 +23,17 @@ class CarService():
             raise BadRequest(CarsCustomExceptions.CAR_ALREADY_EXISTS.value)
         return self.car_repo.add_car(car)
     
-    def get_cars(self):
+    def get_cars(self) -> list[CarsDto]:
         return self.car_repo.get_all_cars()
     
-    def get_car_by_license_plate(self, license_plate):
+    def get_car_by_license_plate(self, license_plate: str) -> CarsDto:
         self._check_if_license_plate_is_present(license_plate)
         car = self.car_repo.get_car_by_license_plate(license_plate)
         if not car:
             raise NotFound(CarsCustomExceptions.CAR_NOT_IN_GARAGE.value)
         return car
 
-    def update_car(self, car: CarsDto):
+    def update_car(self, car: CarsDto) -> CarsDto:
         self._check_if_license_plate_is_present(car.license_plate)
         self._check_if_color_is_present(car.color)
         self._check_if_is_clean_is_present(car.is_clean)
@@ -44,30 +45,30 @@ class CarService():
             raise NotFound(CarsCustomExceptions.CAR_NOT_IN_GARAGE.value)
         return car
     
-    def delete_car(self, license_plate):
+    def delete_car(self, license_plate: str) -> CarsDto:
         self._check_if_license_plate_is_present(license_plate)
         car = self.car_repo.get_car_by_license_plate(license_plate)
         if not car:
             raise NotFound(CarsCustomExceptions.CAR_NOT_IN_GARAGE.value)
         return self.car_repo.delete_car(license_plate)
     
-    def _check_if_license_plate_is_present(self, license_plate):
+    def _check_if_license_plate_is_present(self, license_plate: str) -> None:
         if not license_plate:
             raise BadRequest(CarsCustomExceptions.LICENSE_PLATE_IS_REQUIRED.value)
     
-    def _check_if_color_is_present(self, color) :
+    def _check_if_color_is_present(self, color: str) -> None:
         if not color:
             raise BadRequest(CarsCustomExceptions.COLOR_IS_REQUIRED.value)
     
-    def _check_if_is_clean_is_present(self, is_clean) :
+    def _check_if_is_clean_is_present(self, is_clean: bool) -> None:
         if is_clean is None:
             raise BadRequest(CarsCustomExceptions.IS_CLEAN_IS_REQUIRED.value)
     
-    def _check_if_hours_is_present(self, hours) :
+    def _check_if_hours_is_present(self, hours: int) -> None:
         if not hours:
             raise BadRequest(CarsCustomExceptions.HOURS_IS_REQUIRED.value)
 
-    def calculate_price(self, color, hours, clean):
+    def calculate_price(self, color: str, hours: int, clean: str) -> Union[float, int]:
             if color in [color.value for color in LikedColors]:
                 if clean == True:
                     return 0
