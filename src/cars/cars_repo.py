09fdc9e8681b7cd4aca8bf76e63e4f4cf4fb2362
Stanico <ashadknowles@gmail.com>
2022-07-24 05:02:26 -0400

    create data access layer, define data model

---
+++ src/cars/cars_repo.py
@@ -0,0 +1,42 @@
+from src.cars.cars_model import Car
+from src.cars.dto.cars_dto import CarsDto
+from src.database.database import db
+
+class CarRepo():
+    def __init__(self):
+        self.db = db
+
+    def add_car(self, car: CarsDto):
+        car = Car(car.license_plate, car.color, car.is_clean, car.hours, car.price)
+        self.db.session.add(car)
+        self.db.session.commit()
+        return self.model_object_to_cars_dto(car=car)
+    
+    def get_all_cars(self) -> list[CarsDto]:
+        return list(map(lambda car: self.model_object_to_cars_dto(car=car), Car.query.all()))
+    
+    def get_car_by_license_plate(self, license_plate):
+        car: Car = Car.query.filter_by(license_plate=license_plate).first()
+        if car:
+            return self.model_object_to_cars_dto(car=car)
+        return None
+    
+    def update_car(self, updated_car: CarsDto):
+        car: Car = Car.query.filter_by(license_plate=updated_car.license_plate).first()
+        if car:
+            car.color = updated_car.color
+            car.is_clean = updated_car.is_clean
+            car.hours = updated_car.hours
+            car.price = updated_car.price
+            self.db.session.commit()
+            return self.model_object_to_cars_dto(car=car)
+        return None
+
+    def delete_car(self, license_plate):
+        car = Car.query.filter_by(license_plate=license_plate).first()
+        self.db.session.delete(car)
+        self.db.session.commit()
+        return self.model_object_to_cars_dto(car=car)
+
+    def model_object_to_cars_dto(self, car: Car) -> CarsDto:
+        return CarsDto(car.license_plate, car.color, car.is_clean, car.hours, car.price)
